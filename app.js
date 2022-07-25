cb396d8dcd2603282ccc92b1cb9f529d4acd86f9
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-07-25 19:25:14 +0100

    Made less specific

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("Jan 1, 2023 00:00:00").getTime();
+let countDownDate = new Date("Jan 2, 2023").getTime();
 
 let x = setInterval(function () {
   let now = new Date().getTime();
@@ -7,16 +7,10 @@ let x = setInterval(function () {
 
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("days").innerHTML = days + " DAYS ";
 
   document.getElementById("hours").innerHTML = hours + " HOURS ";
-
-  document.getElementById("minutes").innerHTML = minutes + " MINUTES ";
-
-  document.getElementById("seconds").innerHTML = seconds + " SECONDS ";
 
   if (distance < 0) {
     clearInterval(x);
